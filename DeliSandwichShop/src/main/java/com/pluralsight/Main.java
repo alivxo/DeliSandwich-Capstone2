752785c1f831910e38e383f36f37b4797d805157
Alivxo <avsal0218@gmail.com>
2024-05-24 14:50:25 -0400

    Added code

--- DeliSandwichShop/src/main/java/com/pluralsight/Main.java
+++ DeliSandwichShop/src/main/java/com/pluralsight/Main.java
@@ -4,6 +4,14 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String homeScreen = """
+                Welcome to Ice Spice Deli
+                1. Bacon Egg and Cheese 
+                2. Butter roll
+                3. Chopped Cheese
+                4. Bagel and Cream Cheese
+                5. Egg and Cheese on a roll
+                """;
 
         }
     }
