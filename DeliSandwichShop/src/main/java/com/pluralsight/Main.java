7a2325f4efb40b39bffa2b5ef21d3341044fcfca
Alivxo <avsal0218@gmail.com>
2024-05-24 15:55:49 -0400

    Added code

--- DeliSandwichShop/src/main/java/com/pluralsight/Main.java
+++ DeliSandwichShop/src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,146 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static final Scanner scanner = new Scanner (System.in);
     public static void main(String[] args) {
-        String homeScreen = """
-                Welcome to Ice Spice Deli
-                1. Bacon Egg and Cheese 
-                2. Butter roll
-                3. Chopped Cheese
-                4. Bagel and Cream Cheese
-                5. Egg and Cheese on a roll
+        homeScreen();
+    }
+        public static void homeScreen() {
+
+            boolean choice = true;
+
+            while (choice) {
+                String homeScreen = """
+                        Welcome to Ice Spice Deli
+                        1. New Order
+                        0. Exit - exit the application
+                        """;
+                System.out.println(homeScreen);
+                System.out.print("Please choose from the following: ");
+                String userchoice = scanner.nextLine();
+
+                switch (userchoice) {
+                    case "1", "New Order", "new order":
+                        orderScreen();
+                        break;
+                    case "exit", "Exit", "0":
+                        choice = false;
+                        break;
+                    default:
+                        System.out.println("Sorry Spice didn't get that");
+                }
+            }
+        }
+
+    private static void orderScreen() {
+        String deliScreen = """
+                Welcome to Ice Spice Deli Screen
+                1. Add Sandwich
+                2. Add Drink
+                3. Add Chips
+                4. Checkout
+                0. Cancel Order
                 """;
+        System.out.println(deliScreen);
+        System.out.print("Choose from the following: ");
+        String userChoice = scanner.nextLine();
+
+        boolean choice = true;
+        switch (userChoice){
+            case "1", "add sandwich", "Add Sandwich": addSandwich();
+            break;
+            case "2", "add drink", "Add Drink": addDrink();
+            break;
+            case "3", "add chips", "Add Chips": addChips();
+            break;
+            case "4", "checkout", "Checkout": checkout();
+            break;
+            case "0", "exit", "Exit": homeScreen();
+            break;
+            default:
+                System.out.println("Please choose valid answer");
+
 
         }
+
+
     }
+
+    private static void addSandwich() {
+        String sandwichDisplay = """
+                __________________________________________
+                SANDWICH PRICES | 4"     | 8"     | 12"
+                _________________________________________
+                Bread           | $5.50  | $7.00  | $8.50
+                - White
+                - Wheat
+                - Rye
+                - Wrap
+                __________________________________________
+                TOPPINGS        | 4"     | 8"     | 12"
+                -----------------------------------------
+                Meats           | $1.00  | $2.00  | $3.00
+                - Steak         |        |        |     
+                - Ham           |        |        |     
+                - Salami        |        |        |     
+                - Roast Beef    |        |        |     
+                - Chicken       |        |        |     
+                - Bacon         |        |        |     
+                __________________________________________
+                Extra Meat      | $.50   | $1.00  | $1.50
+                __________________________________________
+                Cheese          | $.50   | $1.50  | $2.25
+                - American      |        |        |     
+                - Provolone     |        |        |     
+                - Cheddar       |        |        |     
+                - Swiss         |        |        |  
+                __________________________________________ 
+                Extra Cheese    | $.30   | $.60   | $.90
+                __________________________________________
+                REGULAR TOPPINGS|     Included    |
+                - Lettuce
+                - Peppers 
+                - Onions
+                - Tomatoes
+                - Jalapenos
+                - Cucumbers
+                - Pickles
+                - Guacamole
+                - Mushrooms
+                __________________________________________
+                Sauces 
+                - Mayo
+                - Mustard
+                __________________________________________
+                __________________________________________
+                
+                
+                
+                
+                
+                
+                """;
+        System.out.println(sandwichDisplay);
+
+
+    }
+
+    private static void addDrink() {
+
+
+    }
+
+    private static void addChips() {
+
+
+    }
+
+    private static void checkout() {
+
+
+    }
+}
---
+++ DeliSandwichShop/src/main/java/com/pluralsight/sandwich/Sandwich.java
@@ -0,0 +1,5 @@
+package com.pluralsight.sandwich;
+
+public class Sandwich {
+
+}
