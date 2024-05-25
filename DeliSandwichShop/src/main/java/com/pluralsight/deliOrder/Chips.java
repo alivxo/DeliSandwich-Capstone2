b71f8d7e1c00f09a65fd5bc21c8d9f48438cee41
Alivxo <avsal0218@gmail.com>
2024-05-24 23:14:23 -0400

    added code

--- DeliSandwichShop/src/main/java/com/pluralsight/deliOrder/Chips.java
+++ DeliSandwichShop/src/main/java/com/pluralsight/deliOrder/Chips.java
@@ -1,4 +1,6 @@
 package com.pluralsight.deliOrder;
 
 public class Chips {
+
+
 }
--- DeliSandwichShop/src/main/java/com/pluralsight/deliOrder/Drink.java
+++ DeliSandwichShop/src/main/java/com/pluralsight/deliOrder/Drink.java
@@ -1,4 +1,29 @@
 package com.pluralsight.deliOrder;
 
 public class Drink {
+    String drinkFlavor;
+    String sizeDrink;
+
+    public Drink(String drinkFlavor, String sizeDrink) {
+        this.drinkFlavor = drinkFlavor;
+        this.sizeDrink = sizeDrink;
+    }
+
+    public double drinkCost (){
+        double cost = 0;
+        switch (sizeDrink){
+            case "small","Small":
+                return 2.00;
+            case "medium", "Medium":
+                return 2.50;
+            case "large", "Large":
+                return 3.00;
+        }
+        return 0;
+    }
+    @Override
+    public String toString(){
+        return sizeDrink + " "+ drinkFlavor;
+    }
+
 }
