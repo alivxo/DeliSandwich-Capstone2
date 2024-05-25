63c7044a1e52224aec02f75b88e90d029f2a8b8e
Alivxo <avsal0218@gmail.com>
2024-05-24 23:21:09 -0400

    added code

--- DeliSandwichShop/src/main/java/com/pluralsight/deliOrder/Chips.java
+++ DeliSandwichShop/src/main/java/com/pluralsight/deliOrder/Chips.java
@@ -1,6 +1,18 @@
 package com.pluralsight.deliOrder;
 
 public class Chips {
+    String chipType;
+
+    public Chips (String chipType){
+        this.chipType = chipType;
+    }
+    public double chipCost (){
+        return 1.50;
+    }
+    @Override
+    public String toString (){
+        return chipType;
+    }
 
 
 }
