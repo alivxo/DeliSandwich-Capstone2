bd0a479c515976cd09bec3bcc2abb577dd5bcf81
Alivxo <avsal0218@gmail.com>
2024-05-28 10:21:51 -0400

    added code

--- DeliSandwichShop/src/main/java/com/pluralsight/Main.java
+++ DeliSandwichShop/src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import com.pluralsight.deliOrder.Drink;
 import com.pluralsight.deliOrder.Sandwich;
 import com.pluralsight.deliOrder.SandwichOrder;
 
@@ -43,7 +44,7 @@ public class Main {
     private static void orderScreen() {
         SandwichOrder order = new SandwichOrder();
         String deliScreen = """
-                Welcome to Ice Spice Deli Screen
+                Welcome to Ice Spice Deli
                 1. Add Sandwich
                 2. Add Drink
                 3. Add Chips
@@ -56,11 +57,11 @@ public class Main {
 
         boolean choice = true;
         switch (userChoice){
-            case "1", "add sandwich", "Add Sandwich": addSandwich();
+            case "1", "add sandwich", "Add Sandwich": addSandwich(order);
             break;
-            case "2", "add drink", "Add Drink": addDrink();
+            case "2", "add drink", "Add Drink": addDrink(order);
             break;
-            case "3", "add chips", "Add Chips": addChips();
+            case "3", "add chips", "Add Chips": addChips(order);
             break;
             case "4", "checkout", "Checkout": checkout();
             break;
@@ -75,22 +76,22 @@ public class Main {
 
     }
 
-    private static void addSandwich() {
+    private static void addSandwich(SandwichOrder order) {
         String sandwichDisplay = """
                 
                 ____________________________________________
                 |SANDWICH PRICES | 4"     | 8"     | 12"   |
                 |__________________________________________|
                 |Bread           | $5.50  | $7.00  | $8.50 |
-                |- White                                   |
-                |- Wheat                                   |
-                |- Rye                                     |
-                |- Wrap                                    |
+                |- White         |        |        |       |
+                |- Wheat         |        |        |       |
+                |- Rye           |        |        |       |
+                |- Wrap          |        |        |       |
                 |__________________________________________|
                 |TOPPINGS        | 4"     | 8"     | 12"   |
                 |------------------------------------------|
                 |Meats           | $1.00  | $2.00  | $3.00 |
-                |- Steak         |        |        |       |     
+                |- Steak         |        |        |       |
                 |- Ham           |        |        |       |     
                 |- Salami        |        |        |       |     
                 |- Roast Beef    |        |        |       |     
@@ -118,23 +119,74 @@ public class Main {
                 |- Guacamole                               |
                 |- Mushrooms                               |
                 |__________________________________________|
-                |Sauces          |     Included    |       |
+                |SAUCES          |     Included    |       |
                 |- Mayo                                    |
-                |- Mustard                                 |
+                |- Mustard                                 | 
+                |- Ketchup                                 |
+                |- Ranch                                   |
+                |-Thousands Islands                        |
+                |-Vinaigrette                              |
                 |__________________________________________|
+                |SIDES           |     Included    |       |
+                |-AU Jus
+                |-Sauce
+                ___________________________________________|
+                |OTHER PRODUCTS|Small  | Medium    |Large  |
+                |Drinks        |2.00   |2.50       |3.00   |
+                -------------------------------------------|
+                |Chips                1.50                 |
                 |__________________________________________|
                 """;
         System.out.println(sandwichDisplay);
 
+        System.out.print("What bread size would you like?:  ");
+        int breadSize = scanner.nextInt();
+        scanner.nextInt();
+
+        System.out.print("What type of bread would you like?: ");
+        String breadChoice = scanner.nextLine();
+
+        System.out.print("What type of meat would you like?:  ");
+        String meatChoice = scanner.nextLine();
+
+        System.out.print("Would you like extra meat?");
+        String extraMeat = scanner.nextLine();
+
+        System.out.print("What type of cheese would you like?: ");
+        String cheeseChoice = scanner.nextLine();
+
+        System.out.print("Would you like extra cheese?: ");
+        String extraCheese = scanner.nextLine();
+
+        System.out.print("Would you like any sauce?: ");
+        String sauceChoice = scanner.nextLine();
+
+        System.out.println("Would you like your sandwich toasted? (yes/no): ");
+        boolean toastedBread = scanner.nextLine().equalsIgnoreCase("yes");
+
+        Sandwich sandwich = new Sandwich(breadSize,breadChoice,meatChoice,
+                            cheeseChoice,extraMeat,extraCheese,toastedBread);
+        order.addSandwich(sandwich);
+        System.out.println("Sandwich added.");
+
 
     }
 
-    private static void addDrink() {
+    private static void addDrink(SandwichOrder order) {
+        System.out.println("What size drink would you like: ");
+        String drinkSizeChoice = scanner.nextLine();
 
+        System.out.println("What flavor drink would you like?: ");
+        String drinkFlavorChoice = scanner.nextLine();
+
+        Drink drink = new Drink(drinkFlavorChoice,drinkSizeChoice);
+        order.addDrink(drink);
+        System.out.println("Drink Added.");
 
     }
 
-    private static void addChips() {
+    private static void addChips(SandwichOrder order) {
+
 
 
     }
